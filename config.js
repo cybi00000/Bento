db086a8ccd6bf9e9c72243a37cb2d12c2d7e2ef6
cybi00000 <68504145+cybi00000@users.noreply.github.com>
2022-07-09 14:13:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '3806810301ae9d9e2955a87f7bb35929', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'White', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
